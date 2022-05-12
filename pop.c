39aa5245aa25d4106dc192206a0134e275118234
LucileSD <4316@holbertonschool.com>
2022-05-12 09:54:02 +0000

    pop

--- pop.c
+++ pop.c
@@ -11,7 +11,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop an empty stack", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		error = 1;
 		return;
 	}
