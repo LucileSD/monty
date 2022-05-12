2e53747d42c5a663c3f8fd3d4695797b21ec4a9d
LucileSD <4316@holbertonschool.com>
2022-05-12 11:35:37 +0000

    sub

--- sub.c
+++ sub.c
@@ -19,7 +19,7 @@ void _sub(stack_t **stack, unsigned int line_number)
 	}
 	else
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		error = 1;
 		return;
 	}
